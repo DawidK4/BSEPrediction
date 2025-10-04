23d34fa6b1d12057bf925689f0b8f00d8bb5e91b
DawidK4 <s27637@pjwstk.edu.pl>
2025-10-04 21:00:07 +0200

    ohlcv and get_stock_data

--- utils/fetch_and_save_data.py
+++ utils/fetch_and_save_data.py
@@ -1,34 +1,42 @@
 import pandas as pd
 import yfinance as yf
 
-def fetch_price(ticker: str, period: str='max', interval: str='1d') -> list[pd.DataFrame]:
+def get_price_and_volume(ticker: str, period: str='max', interval: str='1d') -> pd.DataFrame:
     """
-    Fetch various stock data for a given ticker.
+    Fetch OHLC and volume data for a given stock ticker.
+
     Parameters:
     ticker (str): Stock ticker symbol.
     period (str): Data period to download (e.g., '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max').
     interval (str): Data interval (e.g., '1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo').
+
     Returns:
-    list[pd.DataFrame]: List containing DataFrames of OHLCV, dividends, splits, options, and actions.
+    pd.DataFrame: DataFrame containing OHLC and volume data.
     """
-    result = []
+    data = yf.download(ticker, period=period, interval=interval)
 
-    ohlcv = yf.download(ticker, period=period, interval=interval)
-    result.append(ohlcv)
+    return data
 
+def get_stock_data(ticker: str) -> list[pd.DataFrame]:
+    """
+    Fetch various stock data for a given ticker.
+
+    Parameters:
+    ticker (str): Stock ticker symbol.
+
+    Returns:
+    list[pd.DataFrame]: List containing DataFrames of dividends, splits, and capital gains.
+    """
     stock = yf.Ticker(ticker)
 
+    isin = stock.isin
+    history = stock.history(period='max')
     dividends = stock.dividends
-    result.append(dividends)
-
     splits = stock.splits
-    result.append(splits)
-
-    options = stock.options
-    result.append(options)
-
     actions = stock.actions
-    result.append(actions)
+    capital_gains = stock.capital_gains
+    info = pd.Series(stock.info)
+    fast_info = pd.Series(stock.fast_info)
+    news = stock.news
 
-    return result
-
+    return [isin, history, dividends, splits, actions, capital_gains, info, fast_info, news]
