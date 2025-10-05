20530c0ac6e602fe5cf831b87e91ca61673cc4ab
DawidK4 <s27637@pjwstk.edu.pl>
2025-10-05 15:10:15 +0200

    changed filepaths

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import utils
 
 if __name__ == "__main__":
-    utils.download_and_save_data(ticker="AAPL")
-    utils.download_and_save_data(ticker="AMZN")
+    # utils.download_and_save_data(ticker="AAPL")
+    # utils.download_and_save_data(ticker="AMZN")
+    pass
