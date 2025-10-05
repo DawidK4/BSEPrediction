4aa0745789ead9be3c1234f4724399ecc68c73b4
DawidK4 <s27637@pjwstk.edu.pl>
2025-10-05 14:47:29 +0200

    updated init.py in utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,5 @@ from .fetch_and_save_data import *
 
 __version__ = "1.0.0"
 __author__ = "Dawid Kucharski"
-__all__ = []
+__all__ = ["get_price_and_volume", "get_stock_data", "get_financials", "get_analysis_and_holdings",
+           "download_and_save_data"]
