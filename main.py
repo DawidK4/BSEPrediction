d9bd7abe9b1f8ca7e0311fed1a0f97417903839c
DawidK4 <s27637@pjwstk.edu.pl>
2025-10-08 18:39:09 +0200

    added download_and_save_VIX

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import utils
 if __name__ == "__main__":
     # utils.download_and_save_data(ticker="AAPL")
     # utils.download_and_save_data(ticker="AMZN")
+    # res = utils.download_and_save_VIX()
+    # print(res)
     pass
